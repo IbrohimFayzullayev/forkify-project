3b92e293e8e8c777c2571ea1d051234e1dba6d1b
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-13 18:41:43 +0500

    helper and config file create and write

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 5;
+
+// bu faylda har doim ozgramaydigan qiymatlarni saqlab qoyib shu qiymatlardan foydalanamiz
+// qachon ozgartirish kerak bolib qolsa shu joydan  ozgartirsak hamma joyda ozgaraveradi
