57f18c17a4f33fcb7455e8a8f0cf836b0244733d
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-14 20:18:54 +0500

    new future

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,3 +1,5 @@
+import icons from '../../img/icons.svg'; // Parcel 1-versiya
+
 class SearchView {
   #parentElement = document.querySelector('.search');
 
@@ -6,11 +8,30 @@ class SearchView {
     return val;
   }
 
+  #clearHtml() {
+    document.querySelector('.search-results').innerHTML = '';
+  }
+
   addHandlerEvent(handle) {
     this.#parentElement.addEventListener('submit', function (e) {
       e.preventDefault();
       handle();
     });
+  }
+
+  setError() {
+    const html = `<div class="error">
+    <div>
+      <svg>
+        <use href="${icons}.svg#icon-alert-triangle"></use>
+      </svg>
+    </div>
+    <p>Malumot topilmadi </p>
+  </div>`;
+    this.#clearHtml();
+    document
+      .querySelector('.search-results')
+      .insertAdjacentHTML('afterbegin', html);
   }
 }
 export default new SearchView();
