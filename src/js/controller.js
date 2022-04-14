d7ac660db3ca2fd6ce77fc8bb50a2f7ce543cd7b
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-14 20:18:11 +0500

    new function add

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,10 @@
 const { async } = require('regenerator-runtime');
 
-import { loadRecipe, searchResults, state } from './model.js';
+import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -16,9 +17,9 @@ const showRecipe = async function () {
 
     recipeView.loadingSpinner();
 
-    await loadRecipe(id);
+    await model.loadRecipe(id);
 
-    const data = state.recipe;
+    const data = model.state.recipe;
 
     recipeView.render(data);
   } catch (err) {
@@ -28,13 +29,38 @@ const showRecipe = async function () {
 };
 
 const searchController = async function () {
-  const inputValue = searchView.getQuery();
+  try {
+    const inputValue = searchView.getQuery();
 
-  await searchResults(inputValue);
+    await model.searchResults(inputValue);
 
-  const data = state.search.results;
-  resultsView.render(data);
+    const data = model.paginationLogic();
+
+    paginationView.render(model.state.search);
+
+    resultsView.render(data);
+  } catch (err) {
+    searchView.setError();
+    throw err;
+  }
 };
+
+const paginationController = async function (n) {
+  try {
+    if (n === 1) {
+      ++model.state.search.page;
+    } else {
+      --model.state.search.page;
+    }
+    const data = model.paginationLogic();
+    paginationView.render(model.state.search);
+    resultsView.render(data);
+  } catch (err) {
+    throw err;
+  }
+};
+
+paginationView.addHandlerEvent(paginationController);
 
 searchView.addHandlerEvent(searchController);
 
